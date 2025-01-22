b3e8495ea9f12353034a6d8dee2e48d67d35f613
ssp24 <62755563+ssp24@users.noreply.github.com>
2025-01-22 13:53:05 +0100

    Update get_urn_stats.py

--- get_urn_stats.py
+++ get_urn_stats.py
@@ -7,7 +7,8 @@ def get_all():
     r1 = requests.get("https://api.nbn-resolving.org/v2/urns")
     data = r1.json()
     df = pd.json_normalize(data)
-    total_urns = str(df['totalItems'][0])
+    total_urns = df['totalItems'][0]
+    total_urns = str(total_urns)
         
     return(total_urns)
 
@@ -17,7 +18,7 @@ current_time = current_time.strftime('%Y-%m-%d %H:%M')
 
 urn_all = get_all()
 
-text = current_time +", " + urn_all + "\n"
+text = current_time + ", " + urn_all + "\n"
 
 with open("data/stats.txt", 'a') as outfile:
     outfile.write(text)
