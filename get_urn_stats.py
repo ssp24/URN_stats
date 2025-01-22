27c111e2d84ab6d8fd0d72c673553491297e9a79
ssp24 <62755563+ssp24@users.noreply.github.com>
2025-01-22 22:46:14 +0100

    Update get_urn_stats.py

--- get_urn_stats.py
+++ get_urn_stats.py
@@ -4,21 +4,32 @@ import datetime
 
 def get_all():
     
-    r1 = requests.get("https://api.nbn-resolving.org/v2/urns")
-    data = r1.json()
-    df = pd.json_normalize(data)
-    total_urns = df['totalItems'][0]
-    total_urns = str(total_urns)
+    	r1 = requests.get("https://api.nbn-resolving.org/v2/urns")
+    	data = r1.json()
+    	df = pd.json_normalize(data)
+    	total_urns = df['totalItems'][0]
+    	total_urns = str(total_urns)
         
-    return(total_urns)
+    	return(total_urns)
+
+def get_all_namespaces(): 
+
+	r2 = requests.get("https://api.nbn-resolving.org/v2/namespaces")
+	namespaces_data = r2.json()
+	df2 = pd.json_normalize(namespaces_data)
+	total_namespaces = df2['totalItems'][0]
+	total_namespaces = str(total_namespaces)
+
+	return(total_namespaces)
 
 
 current_time = datetime.datetime.now()
 current_time = current_time.strftime('%Y-%m-%d %H:%M')
 
 urns = get_all()
+namespaces = get_all_namespaces()
 
-text = current_time + ", " + urns + "\n"
+text = current_time + ", " + urns + ", " + namespaces + "\n"
 
 with open("data/stats.txt", 'a') as outfile:
     outfile.write(text)
