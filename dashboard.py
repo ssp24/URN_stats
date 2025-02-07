7d4e9aacdeed8636f2100d25a68bf7c21d1a28c3
ssp24 <62755563+ssp24@users.noreply.github.com>
2025-02-07 14:37:25 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -70,9 +70,10 @@ st.plotly_chart(fig0)
 st.subheader("Gesamt") 
 all_data = [df_old, df]
 df_concat = pd.concat(all_data)
-fig1 = px.line(df_concat, x='time', y='total-urns', title="Anzahl registrierter URNs seit 2003", height=750, color_discrete_sequence=["#1e2a9c"],
+df_concat['time_short'] = df_concat['time'].str[:10]
+fig1 = px.line(df_concat, x='time_short', y='total-urns', title="Anzahl registrierter URNs seit 2003", height=750, color_discrete_sequence=["#1e2a9c"],
               labels={
-                    "time": "Datum",
+                    "time_short": "Datum",
                     "total-urns": "Anzahl URNs"
               })
 st.plotly_chart(fig1) 
